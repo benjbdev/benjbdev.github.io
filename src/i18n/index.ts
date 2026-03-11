5098b76c0c84b2b0ff01c452ed03c0403e160c8c
benjbdev <benjbdev@users.noreply.github.com>
2026-03-11 15:10:29 +0000

    deploy: c7d096c04ae8eabc8690e4568e74e1065cf28be5

--- src/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-import { en } from './en';
-import { fr } from './fr';
-
-export type Lang = 'en' | 'fr';
-export type { Translations } from './en';
-
-export function useTranslations(lang: Lang) {
-  return lang === 'fr' ? fr : en;
-}
